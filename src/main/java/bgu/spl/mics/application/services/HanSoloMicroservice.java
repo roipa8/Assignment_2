422d2555c2f887ad9fe89f5dfe6cba1c4d013ff6
schaked <schaked@post.bgu.ac.il>
2020-11-30 20:11:29 +0200

    a

--- src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
@@ -3,14 +3,14 @@ package bgu.spl.mics.application.services;
 
 import bgu.spl.mics.MicroService;
 
-/**
- * HanSoloMicroservices is in charge of the handling {@link AttackEvents}.
- * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
- *
- * You can add private fields and public methods to this class.
- * You MAY change constructor signatures and even add new public constructors.
- */
+///**
+// * HanSoloMicroservices is in charge of the handling {@link AttackEvents}.
+// * This class may not hold references for objects which it is not responsible for:
+// * {@link AttackEvents}.
+// *
+// * You can add private fields and public methods to this class.
+// * You MAY change constructor signatures and even add new public constructors.
+// */
 public class HanSoloMicroservice extends MicroService {
 
     public HanSoloMicroservice() {
@@ -22,4 +22,7 @@ public class HanSoloMicroservice extends MicroService {
     protected void initialize() {
 
     }
+
+
+
 }
